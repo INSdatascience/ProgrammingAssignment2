8e6e39f457e29a320f3f18432ada9eabfbcd20ed
michaelhfwu <INSdatascience@users.noreply.github.com>
2015-10-23 14:30:29 -0400

    coursera prog assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Essential idea is to create in "makeCacheMatrix" a matrix of functions 
+## that will be used to calculate matrix inverses and remember them for later use.
 
-## Write a short comment describing this function
+## key elements is that the set function resets m to NULL, so anytime a new matrix is inputted, the cache is emptied.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setInv <- function(Inv) m <<- Inv
+      getInv <- function() m
+      list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
-
-## Write a short comment describing this function
+## if conditional checks if there is something in m, and if so, print it. If not, then calculate it, cache it then print it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getInv()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setInv(m)
+      m
 }
